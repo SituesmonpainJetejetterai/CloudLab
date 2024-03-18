1c493fda7c42a65bc6c6ba53fad588debe8f3eb7
SituesmonpainJetejetterai <situmonpainjetejetterai@gmail.com>
2024-03-18 09:30:17 -0500

    refactor: formatting

--- variables.tf
+++ variables.tf
@@ -1,15 +1,12 @@
 variable "ec2_instance_profile" {
   type = string
 }
-
 variable "ec2_instance_role" {
   type = string
 }
-
 variable "ec2_role_path" {
   type = string
 }
-
 variable "public_key_path" {
   type = string
 }
