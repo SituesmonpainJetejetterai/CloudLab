a8a47a6b38d66f8e1200ac4cc604c8abaa022d84
SituesmonpainJetejetterai <situmonpainjetejetterai@gmail.com>
2024-03-18 17:05:54 -0500

    refactor: ec2-specific variables for use with modules

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,41 @@
+variable "ec2_instance_profile_k8s" {
+  type = string
+}
+variable "ec2_instance_role_k8s" {
+  type = string
+}
+
+# -----------------
+
+variable "ec2_role_path" {
+  type = string
+}
+
+# -----------------
+
+variable "public_key_path" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+# -----------------
+
+variable "ec2_instance_profile_saltstack" {
+  type = string
+}
+variable "ec2_instance_role_saltstack" {
+  type = string
+}
+
+# -----------------
+
+variable "subnet_id" {}
+
+variable "security_groups" {}
