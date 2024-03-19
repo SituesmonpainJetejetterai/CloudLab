7cfce2242477d4c3334658247062a7725359fddc
SituesmonpainJetejetterai <situmonpainjetejetterai@gmail.com>
2024-03-19 01:46:15 -0500

    docs: vpc variables to define by module

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_vpc_ipam_pool_cidr" {
+  type = string
+}
+
+variable "ssh_from_port" {
+  type = string
+}
+
+variable "ssh_to_port" {
+  type = string
+}
+
+variable "https_from_port" {
+  type = string
+}
+
+variable "https_to_port" {
+  type = string
+}
+
+variable "dns_from_port" {
+  type = string
+}
+
+variable "dns_to_port" {
+  type = string
+}
+
+variable "saltstack_from_port" {
+  type = string
+}
+
+variable "saltstack_to_port" {
+  type = string
+}
