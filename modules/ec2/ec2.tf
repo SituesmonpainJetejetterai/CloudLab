bb5da77aecf10f3036e3ba5b0204272402a7ea4d
SituesmonpainJetejetterai <situmonpainjetejetterai@gmail.com>
2024-04-09 16:26:23 -0500

    refactor: update user-data, remove saltstack master (test)

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -4,13 +4,13 @@
 # Set up cloud-init directives to set up a user, download necessary packages (git, vim, salt etc) and other pre-orchestration tooling
 
 resource "aws_instance" "ec2_instance_k8s_master" {
-  count = 3
+  count = 1
   launch_template {
     id      = aws_launch_template.ec2_instance_launch_template_k8s.id
     version = "$Latest"
   }
 
-  user_data = templatefile("${path.module}/cloud-init/k8s-master_salt-minion", {salt_master_dns = aws_instance.ec2_instance_saltstack_master.private_dns})
+  user_data = templatefile("${path.module}/scripts/k8s.sh", {node_name="controlplane", count="${count.index}"})
   user_data_replace_on_change = true
 
   tags = {
@@ -25,7 +25,7 @@ resource "aws_instance" "ec2_instance_k8s_worker" {
     version = "$Latest"
   }
 
-  user_data = templatefile("${path.module}/cloud-init/k8s-worker_salt-minion", {salt_master_dns = aws_instance.ec2_instance_saltstack_master.private_dns})
+  user_data = templatefile("${path.module}/scripts/k8s.sh", {node_name="worker", count="${count.index}"})
   user_data_replace_on_change = true
 
   tags = {
@@ -33,27 +33,10 @@ resource "aws_instance" "ec2_instance_k8s_worker" {
   }
 }
 
-resource "aws_instance" "ec2_instance_saltstack_master" {
-  launch_template {
-    id      = aws_launch_template.ec2_instance_launch_template_saltstack_master.id
-    version = "$Latest"
-  }
-
-  user_data = templatefile("${path.module}/cloud-init/salt-master", {private_subnet_cidr = var.private_subnet_cidr})
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "saltstack-master"
-  }
-}
-
 # ---------------------
 # Output public DNS of EC2
 # https://developer.hashicorp.com/terraform/language/meta-arguments/count#referring-to-instances
 
-output "ec2_instance_public_dns_saltstack_master" {
-  value = aws_instance.ec2_instance_saltstack_master.public_dns
-}
 output "ec2_instance_public_dns_master" {
   value = aws_instance.ec2_instance_k8s_master[*].public_dns
 }
